24ee0e495ae084171799779394446a6556607686
yume <yume101099@gmail.com>
2025-06-29 03:29:52 +0800

    feat: 添加INode定义

--- src/core/inode.h
+++ src/core/inode.h
@@ -1,8 +1,18 @@
-//
-// Created by 28396 on 2025/6/29.
-//
-
 #ifndef INODE_H
 #define INODE_H
+#include <cstdint>
+#include <ctime>
+
+struct INode {
+    uint32_t id;                    // 节点ID
+    uint8_t type;                   // 类型（文件/目录）
+    uint32_t size;                  // 文件大小
+    uint32_t start_block;           // 起始块号
+    uint32_t block_count;           // 占用块数
+    uint32_t parent_id;             // 父目录ID
+    time_t create_time;             // 创建时间
+    time_t modify_time;             // 修改时间
+    char name[64];                  // 文件名
+};
 
 #endif //INODE_H
